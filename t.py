1f2fcd8182a6fa2847a87bf0fe0c23ab03c5f314
abdelilah123A <a.elgharbaoui@mundiapolis.ma>
2024-02-26 22:48:16 +0100

    tes

--- t.py
+++ t.py
@@ -15,39 +15,35 @@ def main():
     uploaded_file1 = st.sidebar.file_uploader("Choose another file", type=["xlsx", "xls"])
 
     if uploaded_file2 is not None and uploaded_file1 is not None:
-
         decomp_data = pd.read_excel(uploaded_file1)
         forecast_data = pd.read_excel(uploaded_file2)
 
-        grouped_decomp = decomp_data.groupby('Variant')['Number'].apply(lambda x: ','.join(str(i) for i in x)).reset_index()
-        xs = grouped_decomp.drop_duplicates(subset='Number')
+        # Group by and concatenate values in 'Number' column for xs
+        xs = decomp_data.groupby('Variant')['Number'].apply(lambda x: ','.join(str(i) for i in x)).reset_index().drop_duplicates(subset='Number')
 
-        # Drop duplicates for ys
+        # ys is forecast_data
         ys = forecast_data
 
         def sequence_same_elements(seq1, seq2):
-            return set(seq1) == set(seq2)
+            return np.array_equal(np.sort(seq1), np.sort(seq2))
 
-        df1 = xs
-        df2 = ys
+        for index, row in ys.iterrows():
+            wh_values = np.array(list(map(int, row['WH'].split(','))))
 
-        df1['Variant'] = df1['Variant'].astype('object')
-        for index, row in df2.iterrows():
-            wh_values = list(map(int, row['WH'].split(',')))  
-            for index_1, row_1 in df1.iterrows():
-                module_numbers = list(map(int, row_1['Number'].split(','))) 
-        
+            for index_1, row_1 in xs.iterrows():
+                module_numbers = np.array(list(map(int, row_1['Number'].split(','))))
+
                 if sequence_same_elements(wh_values, module_numbers):
-                    df2.at[index, 'Variant'] = row_1['Variant']
-                    break 
-        
-        result_df = df2.groupby(['DDATE', 'Variant', 'WH'], as_index=False)['QTY'].sum()
+                    ys.at[index, 'Variant'] = row_1['Variant']
+                    break
 
-        # Filter df2 for rows where 'Variant' is null
-        df2_null = df2[df2['Variant'].isnull()]
+        result_df = ys.groupby(['DDATE', 'Variant', 'WH'], as_index=False)['QTY'].sum()
 
-        # Filter df2 for rows where 'Variant' is not null
-        df2_non_null = df2[df2['Variant'].notnull()]
+        # Filter ys for rows where 'Variant' is null
+        ys_null = ys[ys['Variant'].isnull()]
+
+        # Filter ys for rows where 'Variant' is not null
+        ys_non_null = ys[ys['Variant'].notnull()]
 
         st.write("### Results Variants with total quantity:")
         st.dataframe(result_df)
@@ -59,19 +55,19 @@ def main():
             st.download_button(label='Download Results', data=output_buffer, file_name='results.xlsx', key='download_button')
 
         st.write("### History:")
-        st.dataframe(grouped_decomp)
+        st.dataframe(xs)
 
         st.write("### Forecast:")
         st.dataframe(forecast_data)
 
         st.write("### Results:")
-        st.dataframe(df2)
+        st.dataframe(ys)
 
         st.write("### Results not Null:")
-        st.dataframe(df2_non_null)
+        st.dataframe(ys_non_null)
 
         st.write("### Results Null:")
-        st.dataframe(df2_null)
+        st.dataframe(ys_null)
 
 if __name__ == '__main__':
     main()
